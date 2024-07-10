f699adbed44bd797058469c69da9779649e07b86
vishal1145 <vishal.kumar1145@gmail.com>
2024-07-10 19:50:14 +0530

    changes in profile

--- profile/script.js
+++ profile/script.js
@@ -1,23 +1,64 @@
+// var biznfcApp = angular.module("biznfcApp", []);
+
+// biznfcApp.controller('biznfcController', function ($scope, $http, $location) {
+//     $scope.name = "Rahul Rishikesh";
+
+//     function getQueryParams() {
+//         var params = {};
+//         var queryString = $location.absUrl().split('?')[1];
+//         if (queryString) {
+//             queryString.split('&').forEach(function (param) {
+//                 var pair = param.split('=');
+//                 params[pair[0]] = decodeURIComponent(pair[1]);
+//             });
+//         }
+//         return params;
+//     }
+
+//     var queryParams = getQueryParams();
+//     var requestData = {
+//         "url": queryParams.url
+//     };
+
+//     if (requestData.url) {
+//         $http.post('https://biznfc.net/getCardByUrl', requestData)
+//             .then(function (response) {
+//                 if (response.data.success) {
+//                     $scope.cardDetails = response.data.results[0];
+//                 } else {
+//                     console.error('API response unsuccessful:', response.data);
+//                 }
+//             }, function (error) {
+//                 console.error('Error occurred:', error);
+//             });
+//     } else {
+//         console.error('URL parameter is missing.');
+//     }
+
+//     $scope.shareOnWhatsApp = function () {
+//         if (queryParams.url) {
+//             const message = `Check out this Biznfc card: ${queryParams.url}`;
+//             const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(message)}`;
+//             window.open(whatsappUrl, '_blank');
+//         } else {
+//             console.error('URL parameter is missing.');
+//         }
+//     };
+// });
+
 var biznfcApp = angular.module("biznfcApp", []);
 
 biznfcApp.controller('biznfcController', function ($scope, $http, $location) {
     $scope.name = "Rahul Rishikesh";
 
-    function getQueryParams() {
-        var params = {};
-        var queryString = $location.absUrl().split('?')[1];
-        if (queryString) {
-            queryString.split('&').forEach(function (param) {
-                var pair = param.split('=');
-                params[pair[0]] = decodeURIComponent(pair[1]);
-            });
-        }
-        return params;
+    function getCardIdFromUrl() {
+        var path = $location.absUrl().split('/cards/')[1];
+        return path ? path : null;
     }
 
-    var queryParams = getQueryParams();
+    var cardId = getCardIdFromUrl();
     var requestData = {
-        "url": queryParams.url
+        "url": cardId
     };
 
     if (requestData.url) {
@@ -32,16 +73,16 @@ biznfcApp.controller('biznfcController', function ($scope, $http, $location) {
                 console.error('Error occurred:', error);
             });
     } else {
-        console.error('URL parameter is missing.');
+        console.error('Card ID is missing.');
     }
 
     $scope.shareOnWhatsApp = function () {
-        if (queryParams.url) {
-            const message = `Check out this Biznfc card: ${queryParams.url}`;
+        if (cardId) {
+            const message = `Check out this Biznfc card: https://biznfc.algofolks.com/cards/${cardId}`;
             const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(message)}`;
             window.open(whatsappUrl, '_blank');
         } else {
-            console.error('URL parameter is missing.');
+            console.error('Card ID is missing.');
         }
     };
 });
