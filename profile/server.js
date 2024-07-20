6112357df7b3b6406f64eb167f7850c9edb905aa
vishal1145 <vishal.kumar1145@gmail.com>
2024-07-20 18:49:49 +0530

    add axiso

--- profile/server.js
+++ profile/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const fs = require('fs');
+const axios = require('axios');
 const port = 4200
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
@@ -10,22 +11,59 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/:cardurl', (req, res) => {
+app.get('/:cardurl', async (req, res) => {
   const cardurl = req.params.cardurl;
   console.log({ url: cardurl });
 
-  fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
-      if (err) {
-          console.error('Error reading index.html:', err);
+  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    console.log({ url: cardurl, fullUrl: fullUrl });
+
+  var requestData = {
+    "url": cardurl
+  };
+
+  try {
+      const response = await axios.post(`https://biznfc.net/getCardByUrl`, requestData);
+      const cardData = response.data.results[0];
+
+      //console.log(req);
+
+      //console.log(cardData)
+      fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
+          if (err) {
+              console.error('Error reading index.html:', err);
+              res.status(500).send('Server Error');
+              return;
+          }
+
+          // const dynamicTitle = `${cardData.cardTitle || cardurl}`;
+          var name = cardData.fullName;
+          var designation = cardData.designation;
+          var company = cardData.organization;
+
+          var userTitle = `${name} - ${designation} - ${company} | Biznfc`;
+          data = data.replace('{{userTitle}}', userTitle);
+          data = data.replace('{{userTitle}}', userTitle);
+
+          var getDesc = cardData.bio;
+          data = data.replace('{{getDesc}}', getDesc);
+          data = data.replace('{{getDesc}}', getDesc);
+
+          data = data.replace('{{ogImage}}', cardData.photoUrl);
+
+          data = data.replace('{{ogUrl}}', fullUrl)
+
+          res.send(data);
+      });
+  } catch (error) {
+      if (error.response && error.response.status === 404) {
+          console.error('Card not found:', error.response.status);
+          res.status(404).send('Card not found');
+      } else {
+          console.error('Error fetching card data:', error);
           res.status(500).send('Server Error');
-          return;
       }
-
-      const dynamicTitle = `Page for ${cardurl}`;
-      const modifiedHtml = data.replace('{{userTitle}}', dynamicTitle);
-
-      res.send(modifiedHtml);
-  });
+  }
 });
 
 app.listen(port, () => {
